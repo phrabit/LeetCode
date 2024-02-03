be84366c7e20ec0a240f281da4facb68220ccc07
phrabit <phrabit@gmail.com>
2024-02-03 23:41:20 +0900

    Time: 1020 ms (74.10%), Space: 0B (100.00%) - LeetHub

---
+++ 1873-calculate-special-bonus/1873-calculate-special-bonus.sql
@@ -0,0 +1,9 @@
+# calculate the bonus of each employee.
+# bonus of an employee is 100% of their salary if the ID of the employee is an odd number
+# and the employee's name does not start with the character 'M'
+
+SELECT employee_id,
+       CASE WHEN (employee_id%2 = 1) AND name NOT LIKE "M%" THEN salary
+       ELSE 0 END AS bonus
+FROM Employees
+ORDER BY employee_id ASC;
